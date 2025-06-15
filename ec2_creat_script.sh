0efd46ccea0e8530caeccbd95de022b39f6a1db5
Venkat53253 <venkatratna532@gmail.com>
2025-06-15 15:25:23 +0530

    hello worlds created

--- ec2_creat_script.sh
+++ ec2_creat_script.sh
@@ -14,7 +14,8 @@ fi
 # Check if AWS CLI is installed
 
 
-for#for instance in ${INSTANCES[@]}
+#for instance in ${INSTANCES[@]}
+
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
