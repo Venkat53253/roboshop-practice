5d301024ccf3d6a26f19f67bbcd8e0d13e5bf8c3
Venkat53253 <venkatratna532@gmail.com>
2025-06-15 15:59:00 +0530

    hello worlds created

--- ec2_creat_script.sh
+++ ec2_creat_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="ssg-066d322d0b8ea9c8f" # replace with your SG ID
+SG_ID="sg-066d322d0b8ea9c8f" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z05167558BEIFU213OL8" # replace with your ZONE ID
 DOMAIN_NAME="venaws.site" # replace with your domain
@@ -9,7 +9,7 @@ DOMAIN_NAME="venaws.site" # replace with your domain
 #for instance in ${INSTANCES[@]}
 for instance in "${INSTANCES[@]}"
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids ssg-066d322d0b8ea9c8f --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-066d322d0b8ea9c8f --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
